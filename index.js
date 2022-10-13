290a0ebb82cd1d9cc39baebc1ab8bfbbb1fb6706
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-10-13 22:33:07 +0000

    dates update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-mi_nombre="Marianne"
-mi_edad=25
-es_desarrollador=true
-fecha_nacimiento="09-06-1997"
+mi_nombre="Marianne";
+mi_edad=25;
+es_desarrollador=true;
+fecha_nacimiento=new Date(1997, 5, 9);
 libro_faborito={
   titulo:"El jardín secreto", 
   autor:"Hodgson Burnett", 
-  fecha:"29-10-1924", 
+  fecha:Date(1924, 9, 29)
   url:"https://es.wikipedia.org/wiki/El_jard%C3%ADn_secreto"
 }
+
+console.log(fecha_nacimiento)
