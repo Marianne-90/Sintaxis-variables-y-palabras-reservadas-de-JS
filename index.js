ecde84a4dd48c2c43fcf215ea9610c8d36cb9e24
Marianne Garrido <64554585+Marianne-90@users.noreply.github.com>
2022-10-13 21:55:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+mi_nombre="Marianne"
+mi_edad=25
+es_desarrollador=true
+fecha_nacimiento="09-06-1997"
+libro_faborito={
+  titulo:"El jardín secreto", 
+  autor:"Hodgson Burnett", 
+  fecha:"29-10-1924", 
+  url:"https://es.wikipedia.org/wiki/El_jard%C3%ADn_secreto"
+}
